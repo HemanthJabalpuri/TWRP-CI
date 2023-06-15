3bf285817763bf2681c98f4b02383c107b630b81
Hemanth <hemanthjabalpuri@gmail.com>
2023-06-15 14:03:34 +0530

    test with patches

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ DEVICE_PATH="device/OPPO/OP4B9B"
 COMMON_TREE_URL=""
 COMMON_PATH=""
 BUILD_TARGET="recovery"
-TW_DEVICE_VERSION="test4"
+TW_DEVICE_VERSION="test5"
 
 DEVICE_NAME="$(echo $DEVICE_PATH | cut -d "/" -f 3)"
 case $MANIFEST_BRANCH in
@@ -40,7 +40,12 @@ sync() {
   #cd system/core
   #curl -sL https://github.com/HemanthJabalpuri/twrp_motorola_rhode/files/11550608/dontLoadVendorModules.txt | patch -p 1
   #cd -
-  
+  rm -rf bootable/recovery
+  git clone --depth=1 https://github.com/HemanthJabalpuri/android_bootable_recovery -b android-11 bootable/recovery
+  cd bootable/recovery
+  curl -sL https://github.com/HemanthJabalpuri/android_bootable_recovery/commit/6d5c365617778d107ccc6b32b55238715a06d0bc.patch | patch -p 1
+  cd -
+
   # Clone device tree
   git clone $DEVICE_TREE_URL -b $DEVICE_TREE_BRANCH $DEVICE_PATH || abort "ERROR: Failed to Clone the Device Tree!"
 
