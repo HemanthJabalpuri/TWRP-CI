b26ab068f4df44a220c263fcc5a92fb120417a37
Hemanth <hemanthjabalpuri@gmail.com>
2023-11-22 13:38:18 +0530

    test8

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ DEVICE_PATH="device/motorola/rhode"
 COMMON_TREE_URL=""
 COMMON_PATH=""
 BUILD_TARGET="boot"
-TW_DEVICE_VERSION="test7"
+TW_DEVICE_VERSION="test8"
 
 DEVICE_NAME="$(echo $DEVICE_PATH | cut -d "/" -f 3)"
 case $MANIFEST_BRANCH in
